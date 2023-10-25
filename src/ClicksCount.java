63502a3ac18991043b9dd29e45af0dadd146a5ee
eabock <eabock29@gmail.com>
2023-10-25 11:21:17 -0400

    "moving buttons and coloring"

--- src/ClicksCount.java
+++ src/ClicksCount.java
@@ -11,12 +11,28 @@ class ClicksCount implements ActionListener{
         JFrame frame = new JFrame();
         JButton button1 = new JButton("Button1");
         JButton button2 = new JButton("Button2");
+        JButton button3 =new JButton("X");
         button1.addActionListener(this);
         button2.addActionListener(this::secondButton);
+        button3.addActionListener(this::end);
+
 
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        button1.setBounds(100,100,100,100);
+        button2.setBounds(100, 200, 100, 100);
+        button3.setBounds(400, 0, 30, 30);
+
+        button3.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
+        button3.setHorizontalAlignment(SwingConstants.CENTER);
+        button3.setBackground(Color.RED);
+        button3.setForeground(Color.BLACK);
+        button3.setFont(new Font("ARIAL", Font.PLAIN, 20));
+        frame.setLayout(null);
         frame.add(button1);
         frame.add(button2);
+        frame.add(button3);
+        frame.setTitle("Many Buttons");
+
         frame.getRootPane().setDefaultButton(button1); // sets default button
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(450,450);
@@ -35,8 +51,13 @@ class ClicksCount implements ActionListener{
 
     }
 
+    public void end(ActionEvent ex){
+        System.exit(1);
+    }
+
     public static void main(String args[]){
 
         ClicksCount Clicks = new ClicksCount();
+
     }
 }
